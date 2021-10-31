6c46e957b2310f14fe4e50436157d1d86c850f58
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-11-01 02:20:31 +0300

    solved

---
+++ SeperateSteps/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SeperateSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            var myStack = new Stack<int>();
+
+            while (number>0)
+            {
+                int k = number % 10;
+                myStack.Push(k);
+                number /= 10;
+            }
+
+            int i = 0;
+            int n = myStack.Count-1;
+
+            foreach (var item in myStack)
+            {
+                var stepValue = Math.Pow(10, n - i);
+                Console.WriteLine($"{item} x {stepValue} = {item * stepValue}");
+                i++;
+            }
+
+        }
+    }
+}
